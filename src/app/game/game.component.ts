6fc01765ed46c5676c6dfee1a972f5bee6da0463
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-17 12:02:42 +0100

    added pick card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
 
+  pickCardAnimation = false;
+
+  constructor() { }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+
 }
