55c3d961db033bf9c9059e91e144f74aaca4f8bd
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-20 15:33:36 +0100

    added game title, description and currentPlayer active

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -27,6 +27,9 @@ export class GameComponent implements OnInit {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop() || '';
       this.pickCardAnimation = true;
+
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
