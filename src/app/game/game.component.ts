ded32d7dd55c77f3666cdc4ca871d112367dace1
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-20 15:51:04 +0100

    bug fixes

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -39,9 +39,11 @@ export class GameComponent implements OnInit {
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
-      
+
     dialogRef.afterClosed().subscribe((name: string) => {
-      this.game.players.push(name);
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
     });
   }
 }
