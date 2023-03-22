307b9975f4117e83cc40a0fa593225dc4e844ac3
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-22 14:53:01 +0100

    added responsive startscreen, and new img for startscreen

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBeUAhf94Bi4VOrr5aXq1S2PFZ21p7hMSQ",
+        authDomain: "ring-of-fire-3f640.firebaseapp.com",
+        projectId: "ring-of-fire-3f640",
+        storageBucket: "ring-of-fire-3f640.appspot.com",
+        messagingSenderId: "230388928143",
+        appId: "1:230388928143:web:8939184602440ad4482afa"
+    }
+};
